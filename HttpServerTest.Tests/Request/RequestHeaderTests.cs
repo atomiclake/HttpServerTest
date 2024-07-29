8629952f9732b47f8b9306c868eda4177c2553a9
atomiclake <atomiclake.dev@gmail.com>
2024-07-29 14:35:24 -0300

    Added three test cases to validate the RequestHeader.ToString() functionality

--- HttpServerTest.Tests/Request/RequestHeaderTests.cs
+++ HttpServerTest.Tests/Request/RequestHeaderTests.cs
@@ -5,12 +5,11 @@ namespace HttpServerTest.Tests.Request;
 public class RequestHeaderTests
 {
     private static readonly List<RequestHeader> DefaultTestData =
-        [
-            new("Host", [new("developer.mozilla.org")]),
+    [
+        new("Host", [new("developer.mozilla.org")]),
         new("Accept-Encoding", [new("gzip"), new("deflate"), new("br")]),
         new("Accept", [new("text/html"), new("application/xhtml+xml"), new("application/xml", 0.9f), new("*/*", 0.8f)]),
-        ];
-    }
+    ];
 
     [Fact]
     public void RequestHeader_EmptyName_ShouldThrowException()
@@ -28,5 +27,44 @@ public class RequestHeaderTests
         {
             _ = new RequestHeader("  ");
         });
+    }
+
+    [Fact]
+    public void RequestHeader_SingleValue_ProducesCorrectString()
+    {
+        // Arrange
+        var actualData = DefaultTestData[0].ToString();
+
+        // Act
+        string expectedValue = "Host: developer.mozilla.org";
+
+        // Assert
+        Assert.Equal(expectedValue, actualData, true, true, true, false);
+    }
+
+    [Fact]
+    public void RequestHeader_MultipleValues_ProducesCorrectString()
+    {
+        // Arrange
+        var actualData = DefaultTestData[1].ToString();
+
+        // Act
+        string expectedValue = "Accept-Encoding: gzip,deflate,br";
+
+        // Assert
+        Assert.Equal(expectedValue, actualData, true, true, true, false);
+    }
+
+    [Fact]
+    public void RequestHeader_MultipleValuesWithQuality_ProducesCorrectString_Case1()
+    {
+        // Arrange
+        var actualData = DefaultTestData[2].ToString();
+
+        // Act
+        string expectedValue = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+
+        // Assert
+        Assert.Equal(expectedValue, actualData, true, true, true, false);
     }
 }
