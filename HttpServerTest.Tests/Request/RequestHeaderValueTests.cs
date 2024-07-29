4cefbc219ccafec8bdc56f6ae8549ad51d59c0b0
atomiclake <atomiclake.dev@gmail.com>
2024-07-29 14:50:20 -0300

    Add test class for the request header value funcionality

---
+++ HttpServerTest.Tests/Request/RequestHeaderValueTests.cs
@@ -0,0 +1,42 @@
+﻿using HttpServerTest.Core.Request;
+
+namespace HttpServerTest.Tests.Request;
+
+public class RequestHeaderValueTests
+{
+    [Fact]
+    public void RequestHeaderValue_ValueLessThanZero_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            _ = new RequestHeaderValue("test", -0.1f);
+        });
+    }
+
+    [Fact]
+    public void RequestHeaderValue_ValueGreaterThanOne_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            _ = new RequestHeaderValue("test", 1.01f);
+        });
+    }
+
+    [Fact]
+    public void RequestHeaderValue_EmptyValue_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            _ = new RequestHeaderValue("");
+        });
+    }
+
+    [Fact]
+    public void RequestHeaderValue_BlankValue_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() =>
+        {
+            _ = new RequestHeaderValue("  ");
+        });
+    }
+}
