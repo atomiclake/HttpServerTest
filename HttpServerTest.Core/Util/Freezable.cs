c8390215fc32cd84f24dc8ff4b951c6828bc022b
atomiclake <atomiclake.dev@gmail.com>
2024-07-30 14:32:26 -0300

    Added Util folder for common types and operations

---
+++ HttpServerTest.Core/Util/Freezable.cs
@@ -0,0 +1,49 @@
+﻿namespace HttpServerTest.Core.Util;
+
+public struct Freezable<T>
+{
+    private T _value;
+
+    public bool IsFrozen { get; init; }
+
+    public T Value
+    {
+        readonly get => _value;
+
+        set
+        {
+            if (!IsFrozen)
+            {
+                _value = value;
+            }
+        }
+    }
+
+    public Freezable(T value)
+    {
+        _value = value;
+    }
+
+    public void Freeze()
+    {
+        if (!IsFrozen)
+        {
+            this = this with { IsFrozen = true };
+        }
+    }
+
+    public static Freezable<T> From(T value)
+    {
+        return new(value);
+    }
+
+    public static implicit operator T(Freezable<T> freezable)
+    {
+        return freezable.Value;
+    }
+
+    public static implicit operator Freezable<T>(T value)
+    {
+        return Freezable<T>.From(value);
+    }
+}
